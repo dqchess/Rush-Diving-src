9b563c280e7a5f6334bad2e678e6bee12e24b7c1
shenyi <shenyi.nanjing@gmail.com>
2019-10-29 19:42:48 -0700

    Add Pause Menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+    public GameObject laserPointer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (OVRInput.GetDown(OVRInput.Button.Start))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        laserPointer.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+    
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        laserPointer.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+}
